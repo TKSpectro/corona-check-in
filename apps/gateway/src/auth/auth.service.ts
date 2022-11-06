4ceac7329703b9d5b17cf7befc9bb4178e84e9ef
Tom Kaeppler <tomkaeppler@web.de>
2022-11-06 17:25:45 +0100

    use bcrypt for passwords

--- apps/gateway/src/auth/auth.service.ts
+++ apps/gateway/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { compareSync } from 'bcrypt';
 import { User, UsersService } from '../users/users.service';
 
 @Injectable()
@@ -11,16 +12,18 @@ export class AuthService {
 
   async validateUser(email: string, pass: string): Promise<User | null> {
     const user = await this.usersService.findOne(email);
-    // TODO: Use bcrypt to compare the password
-    if (user && user.password === pass) {
+
+    if (user && compareSync(pass, user.password)) {
       delete user.password;
       return user;
     }
+
     return null;
   }
 
   async login(user: User) {
     const payload = { email: user.email, sub: user.id, roles: user.roles };
+
     return {
       token: this.jwtService.sign(payload),
     };
