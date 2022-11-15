6992adac1278c3692cd90f151cad67de17e92209
Daniel Depta <56829289+ddepta@users.noreply.github.com>
2022-11-15 18:01:50 +0100

    add session card component

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -6,15 +6,19 @@ import { FormsModule } from '@angular/forms';
 import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+
+import { SessionCardComponent } from './dashboard/session-card/session-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SessionCardComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
