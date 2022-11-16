a073dccb32a60846c3501cbb48fc8948c8b6e0d3
Bilal <bilal.alnaani@fh-erfuert.de>
2022-11-16 14:39:33 +0100

    optimize incidence component/service

--- apps/frontend/src/app/shared/server.service.ts
+++ apps/frontend/src/app/shared/server.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServerService {
+  testJwt =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAdHVyYm9tZWV0Lnh5eiIsInN1YiI6Ij' +
+    'AwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMiIsInJvbGVzIjoidXNlciIsImlhdCI6MTY2ODUzODI' +
+    '0MywiZXhwIjoxNjcxMTMwMjQzfQ.5kGUhp8oWctxBMpA_LQW_uLRzGjuhrRWukooX6siouE';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getIncidenceData(): Observable<any> {
+    return this.httpClient.get<any>('/api/incidence', {
+      headers: { Authorization: `Bearer ${this.testJwt}` },
+    });
+  }
 }
