8d90ce69c6ea17a7d36e8c3b47d9c342daabf32f
Tom Kaeppler <tomkaeppler@web.de>
2022-11-16 11:58:58 +0100

    fix lint

--- apps/frontend/src/app/auth/auth.component.ts
+++ apps/frontend/src/app/auth/auth.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ccn-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class AuthComponent {}
