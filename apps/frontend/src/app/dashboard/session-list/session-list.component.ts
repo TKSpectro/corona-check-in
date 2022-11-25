f685b92473702017d426e9c1d2d67b7a40ecf0bb
Daniel Depta <56829289+ddepta@users.noreply.github.com>
2022-11-25 18:01:06 +0100

    get sessions information and display them in a list

--- apps/frontend/src/app/dashboard/session-list/session-list.component.ts
+++ apps/frontend/src/app/dashboard/session-list/session-list.component.ts
@@ -1,28 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
+import { SessionListService } from './session-list.service';
 
 @Component({
   selector: 'ccn-session-list',
   templateUrl: './session-list.component.html',
   styleUrls: ['./session-list.component.scss'],
 })
-export class SessionListComponent {
-  displayedColumns: string[] = ['room', 'starttime', 'endtime'];
+export class SessionListComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = ['name'];
+  sessionData!: any;
+  subscription!: Subscription;
 
-  EmpData: unknown[] = [
-    {
-      id: 1,
-      room: '5.1.05',
-      starttime: '25.10.2022 13:37',
-      endtime: '25.10.2022 16:37',
-    },
-    {
-      id: 2,
-      room: '5.1.05',
-      starttime: '01.11.2022 13:37',
-      endtime: '01.11.2022 13:37',
-    },
-  ];
+  constructor(private sessionListService: SessionListService) {}
 
-  dataSource = new MatTableDataSource(this.EmpData);
+  ngOnInit(): void {
+    // TODO: This will be replaced by a service call
+
+    this.sessionListService.getSessions();
+    this.subscription = this.sessionListService.submitSessionData.subscribe(
+      (data) => {
+        this.sessionData = data;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
