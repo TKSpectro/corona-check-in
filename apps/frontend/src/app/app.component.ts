74c42f9b003ca276a4dc53cbf374546359d7ac9e
Bilal <bilal.alnaani@fh-erfuert.de>
2022-11-30 09:38:24 +0100

    optimize code

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -9,22 +9,25 @@ import { AuthService } from './auth/auth.service';
 })
 export class AppComponent implements OnDestroy, OnInit {
   title = 'frontend';
-
-  ngOnInit() {
-    this.authService.autoLogin();
-  }
-
-  mobileQuery: MediaQueryList;
   opened = true;
+  mobileQuery: MediaQueryList;
 
   fillerNav = Array.from({ length: 7 }, (_, i) => `Nav Item ${i + 1}`);
 
   _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private authService: AuthService) {
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private authService: AuthService
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 700px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit() {
+    this.authService.autoLogin();
   }
 
   ngOnDestroy(): void {
