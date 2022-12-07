90c8923bd060842b51feb9d72c76f0c5f7e69fc0
Daniel Depta <56829289+ddepta@users.noreply.github.com>
2022-12-07 13:13:13 +0100

    create routes for session pages

--- apps/frontend/src/app/app-routing.module.ts
+++ apps/frontend/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent, AuthGuard } from './auth';
 import { DashboardComponent } from './dashboard';
 import { ProfileComponent } from './profile';
+import { SessionDetailsComponent } from './dashboard/session-details/session-details.component';
+import { SessionListComponent } from './dashboard/session-list/session-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -18,6 +20,8 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     component: ProfileComponent,
   },
+  { path: 'sessions/:id', component: SessionDetailsComponent },
+  { path: 'sessions', component: SessionListComponent },
 ];
 
 @NgModule({
