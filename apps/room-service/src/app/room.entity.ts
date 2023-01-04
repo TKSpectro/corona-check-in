30c03ec79ef4fd12c04c848f61cb1744e4f5bb6a
Bilal <bilal.alnaani@fh-erfuert.de>
2023-01-04 11:35:44 +0100

    update room table

--- apps/room-service/src/app/room.entity.ts
+++ apps/room-service/src/app/room.entity.ts
@@ -1,4 +1,17 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { MaxLength, MinLength } from 'class-validator';
+
+export enum Faculty {
+  AI = 'Angewandte Informatik',
+  SA = 'Soziale Arbeit',
+}
 
 @Entity()
 export class RoomEntity extends BaseEntity {
@@ -7,4 +20,29 @@ export class RoomEntity extends BaseEntity {
 
   @Column()
   name: string;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn()
+  updatedDate: Date;
+
+  @MinLength(1)
+  @MaxLength(60)
+  @Column()
+  maxParticipants: number;
+
+  @MinLength(1)
+  @Column()
+  maxDuration: number;
+
+  @Column({
+    type: 'enum',
+    enum: Faculty,
+    default: Faculty.AI,
+  })
+  faculty: Faculty;
+
+  @Column({ nullable: true, type: 'bytea' })
+  qrCode: Uint8Array;
 }
