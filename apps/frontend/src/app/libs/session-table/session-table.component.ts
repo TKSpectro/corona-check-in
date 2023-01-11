fb236ed71df5934cd09699ee9c84b30cf0c548e5
bilal.alnaani@fh-erfurt.de <bilal.alnaani@fh-erfurt.de>
2023-01-11 13:37:17 +0100

    optimize code

--- apps/frontend/src/app/libs/session-table/session-table.component.ts
+++ apps/frontend/src/app/libs/session-table/session-table.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 interface Session {
   id: string;
   name: string;
   startTime: Date;
   endTime?: Date;
+  x;
   infected: boolean;
   note?: string;
 }
@@ -14,11 +15,7 @@ interface Session {
   templateUrl: './session-table.component.html',
   styleUrls: ['./session-table.component.scss'],
 })
-export class SessionTableComponent implements OnInit {
+export class SessionTableComponent {
   @Input() sessionList: Session[] = [];
   displayedColumns: string[] = ['startTime', 'endTime', 'infected'];
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
