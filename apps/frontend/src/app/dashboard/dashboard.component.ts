95f4869f192b6390baf7ae87967e950b8f6dbb80
Tom Kaeppler <tomkaeppler@web.de>
2023-01-16 16:41:30 +0100

    #CCN-55 fix dashboard session-list

--- apps/frontend/src/app/dashboard/dashboard.component.ts
+++ apps/frontend/src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,27 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+import { SessionListService } from '../sessions/session-list.service';
 
 @Component({
   selector: 'ccn-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnDestroy {
+export class DashboardComponent implements OnInit, OnDestroy {
   mobileQuery: MediaQueryList;
   _mobileQueryListener: () => void;
   isExpanded!: boolean;
 
+  sessionListSub!: Subscription;
+  sessionList = [];
+
   constructor(
     public translate: TranslateService,
     media: MediaMatcher,
-    changeDetectorRef: ChangeDetectorRef
+    changeDetectorRef: ChangeDetectorRef,
+    private sessionListService: SessionListService
   ) {
     translate.addLangs(['en', 'de']);
     const browserLang = translate.getBrowserLang();
@@ -31,7 +37,17 @@ export class DashboardComponent implements OnDestroy {
     );
   }
 
+  ngOnInit(): void {
+    this.sessionListSub = this.sessionListService.getSessions(0, 5).subscribe({
+      next: (data) => {
+        this.sessionList = data.data;
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
   ngOnDestroy(): void {
     this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
+    this.sessionListSub.unsubscribe();
   }
 }
