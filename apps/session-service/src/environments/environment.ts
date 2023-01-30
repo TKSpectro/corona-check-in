dd4933bd4c29337419e23ae4a7f9e7b971a94d2b
Tom Kaeppler <tomkaeppler@web.de>
2023-01-30 21:20:40 +0100

    #MC-60 fix env for mass session seeding

--- apps/session-service/src/environments/environment.ts
+++ apps/session-service/src/environments/environment.ts
@@ -1,6 +1,7 @@
 import { baseEnvironment } from '@corona-check-in/micro-service-shared';
 
 export const environment = {
-  seedMassRandom: process.env['SEED_MASS_RANDOM'] === 'true' ? true : false,
+  seedMassRandom:
+    process.env['SEEDING_MASS_SESSIONS_ENABLED'] === 'true' ? true : false,
   ...baseEnvironment,
 };
