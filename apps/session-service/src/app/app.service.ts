02a5721b721e9d5ed80850b809b5dd839bc6ca3a
Tom Kaeppler <tomkaeppler@web.de>
2023-01-31 21:25:27 +0100

    fix scan microservice call

--- apps/session-service/src/app/app.service.ts
+++ apps/session-service/src/app/app.service.ts
@@ -124,7 +124,7 @@ export class AppService implements OnModuleInit {
   ): Promise<SessionEntity> {
     const room = await lastValueFrom(
       this.roomSrv
-        .send({ role: 'room', cmd: 'get-all' }, createSessionDto.roomId)
+        .send({ role: 'room', cmd: 'get-by-id' }, createSessionDto.roomId)
         .pipe(timeout(environment.serviceTimeout))
     );
     if (!room) {
