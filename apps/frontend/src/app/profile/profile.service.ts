1ac7601c41379d7c41418165698907c8e3088c37
Tom Kaeppler <tomkaeppler@web.de>
2023-03-12 15:18:23 +0100

    #CCN-144 rework profile subscriptions

--- apps/frontend/src/app/profile/profile.service.ts
+++ apps/frontend/src/app/profile/profile.service.ts
@@ -1,67 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subject } from 'rxjs';
-import { AuthService } from '../auth/auth.service';
 import { ServerService } from '../shared/server.service';
-import { UpdateUser, User } from '../shared/types';
+import { UpdateUser } from '../shared/types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
-  profileData!: User;
-  submitProfileData = new Subject<User>();
-  updateProfileData = new Subject<User>();
-  deleteUserData = new Subject<User>();
-
-  constructor(
-    private serverSrv: ServerService,
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private serverSrv: ServerService) {}
 
   getProfileData() {
-    this.serverSrv.me().subscribe({
-      next: (result) => {
-        this.profileData = result;
-        this.submitProfileData.next(this.profileData);
-      },
-      error: (error) => {
-        this.submitProfileData.error(error);
-      },
-    });
-
-    return this.profileData;
+    return this.serverSrv.me();
   }
 
   updateUser(id: string, data: UpdateUser) {
-    this.serverSrv.updateUser(id, data).subscribe({
-      next: (result) => {
-        this.profileData = result;
-        this.updateProfileData.next(this.profileData);
-      },
-      error: (error) => {
-        this.updateProfileData.next(error);
-      },
-    });
-
-    return this.profileData;
+    return this.serverSrv.updateUser(id, data);
   }
 
   deleteUser(id: string) {
-    this.serverSrv.deleteUser(id).subscribe({
-      next: (res) => {
-        this.authService.logout();
-
-        this.deleteUserData.next(res);
-      },
-      error: (error) => {
-        this.deleteUserData.next(error);
-      },
-    });
-  }
-
-  logout() {
-    this.authService.logout();
+    return this.serverSrv.deleteUser(id);
   }
 }
